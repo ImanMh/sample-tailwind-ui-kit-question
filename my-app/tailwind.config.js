70f9813021867edaefdebbbe1f4662963ccf41d8
Iman Mohammadi <iman.mohammadi@snapp.cab>
2021-09-26 10:19:47 +0330

    sample created

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html', 'node_modules/ui-kit'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ ui-kit/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export const SampleComponent = () => {
+  return <h1 className='pt-10'>Sample Component</h1>
+}
